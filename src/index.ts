74e9d26ae2fc900ef78b988fae29ff765ce7cc11
JunioDo <poxii2001@gmail.com>
2024-03-07 20:27:12 +0100

    fix: whole content❤️

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import Particles from "./Particles";
+
+export * from "tsparticles/Enums";
+export * from "tsparticles/Plugins/Absorbers/Enums";
+export * from "tsparticles/Plugins/Emitters/Enums";
+export * from "tsparticles/Plugins/PolygonMask/Enums";
+
+export default Particles;
+export { Particles };
